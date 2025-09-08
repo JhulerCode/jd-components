e2368293fe01b21b7a02a5147901737125eec95f
Jhuler Aguirre <jhuler.code@gmail.com>
2025-09-08 18:45:42 -0500

    prueba

--- src/LoadingSpin.vue
+++ src/LoadingSpin.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="spin-load" :class="{ lleno: rellenar, 'back-with-color': shadowBack }"
-        :style="{'border-radius':`${borderRadius}rem`}">
-
-        <div class="container" :style="{ transform: `scale(${scale})`}">
+    <div
+        class="spin-load"
+        :class="{ lleno: rellenar, 'back-with-color': shadowBack }"
+        :style="{ 'border-radius': `${borderRadius}rem` }"
+    >
+        <div class="container" :style="{ transform: `scale(${scale})` }">
             <div class="spinner"></div>
         </div>
 
-        <div class="text" v-if="text!=''">{{ text }}</div>
+        <div class="text" v-if="text != ''">{{ text }}</div>
     </div>
 </template>
 
@@ -18,8 +20,8 @@ export default {
         shadowBack: { type: Boolean, default: true },
         scale: { type: [String, Number], default: 1 },
         borderRadius: { type: [String, Number], default: '0' },
-    }
-}
+    },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -42,7 +44,7 @@ export default {
     background-color: rgba($color: #000000, $alpha: 0.5);
 }
 
-.text{
+.text {
     margin-top: 1rem;
 }
 
